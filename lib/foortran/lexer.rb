82d100310fbbacdeeeba1739d943fe5e8e552e25
Josh Schairbaum <josh.schairbaum@rackspace.com>
2013-01-15 07:22:25 -0600

    Foortran::Lexer tokenizes keywords

--- lib/foortran/lexer.rb
+++ lib/foortran/lexer.rb
@@ -1,8 +1,9 @@
 module Foortran
   class Lexer
-    KEYWORDS = [:program]
+    KEYWORDS = %w(program)
 
     attr_accessor :source
+    attr_reader   :tokens
 
     def self.tokenize(source)
       lexer = new(source)
@@ -11,9 +12,26 @@ module Foortran
 
     def initialize(source)
       self.source = source
+      @tokens     = []
     end
 
     def tokenize
+      i = 0
+      while i < source.size
+        chunk = source[i..-1]
+
+        if identifier = chunk[/\A([a-z]\w*)/, 1]
+          if KEYWORDS.include?(identifier)
+            tokens << [identifier.upcase.to_sym, identifier]
+          end
+
+          i += identifier.size
+        else
+          i += 1
+          next
+        end
+      end
+      tokens
     end
   end
 end
--- spec/foortran/lexer_spec.rb
+++ spec/foortran/lexer_spec.rb
@@ -1,19 +1,27 @@
 require_relative '../spec_helper'
 
 module Foortran
-  describe Lexer do 
+  describe Lexer do
     let(:lexer)  { Lexer.new(source) }
     let(:source) { get_source_file("helloworld.f90") }
 
-    describe ".tokenize" do 
-      it "returns tokenized source" do 
+    describe ".tokenize" do
+      it "returns tokenized source" do
         Lexer.tokenize(source).must_equal lexer.tokenize
       end
     end
 
-    describe ".new" do 
-      it "sets the source attribute" do 
+    describe ".new" do
+      it "sets the source attribute" do
         lexer.source.must_equal source
+      end
+    end
+
+    describe "#tokenize" do
+      let(:tokens) { lexer.tokenize }
+
+      it "returns a keyword token" do
+        tokens.must_include [:PROGRAM, "program"]
       end
     end
   end
